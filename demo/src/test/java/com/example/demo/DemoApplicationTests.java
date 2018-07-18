e1a76459eea51b158799047ee9fcebf13aac3eec
wz14122 <18329030206@163.com>
2018-07-18 17:58:00 +0800

    更换layui表格实现

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.entity.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,10 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		User user = new User("wz", "123");
+		HashMap<String, User> map = new HashMap<>();
+		map.put("1", user);
+		System.out.println(map);
 	}
 
 }
